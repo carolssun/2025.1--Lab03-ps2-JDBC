dd26a73fa3f8e577fd6b95725b62b0fa6bbe980c
carolssun <carolinasuncastilho@gmail.com>
2025-03-10 19:50:34 -0300

    Arrumei o bd

--- lab03/src/main/java/com/example/Main.java
+++ lab03/src/main/java/com/example/Main.java
@@ -2,9 +2,42 @@
 //Millie Talala Zogheib - 10443653
 
 package com.example;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      GerenciadorNomes gNomes = new GerenciadorNomesMem();
-      Ihm ihm = new Ihm(gNomes);
-      ihm.dialogar();
- } }
+        Scanner scanner = new Scanner(System.in);
+        boolean executar = true;
+
+        while (executar) {
+            System.out.println("Escolha o tipo de armazenamento:");
+            System.out.println("(1) Memória");
+            System.out.println("(2) Banco de Dados");
+            System.out.println("(3) Sair");
+            System.out.print("Opção: ");
+            String escolha = scanner.nextLine();
+
+            GerenciadorNomes gNomes = null;
+
+            switch (escolha) {
+                case "1":
+                    gNomes = new GerenciadorNomesMem();
+                    break;
+                case "2":
+                    gNomes = new GerenciadorNomesBD();
+                    break;
+                case "3":
+                    System.out.println("Saindo do programa...");
+                    executar = false;
+                    continue;  // Volta ao início do loop sem executar `Ihm`
+                default:
+                    System.out.println("Opção inválida, tente novamente.");
+                    continue;
+            }
+
+            Ihm ihm = new Ihm(gNomes);
+            ihm.dialogar();
+        }
+        scanner.close();
+    }
+}
